7bbff611d43d307ff98578b2e76c4b202e0b5768
lilkjo <100037748+lilkjo@users.noreply.github.com>
2022-12-08 13:34:57 +0100

    Add files via upload

---
+++ assets/amdiSettings.js
@@ -0,0 +1,33 @@
+/**
+░██████╗░██╗░░░██╗███████╗███████╗███╗░░██╗  ░█████╗░███╗░░░███╗██████╗░██╗
+██╔═══██╗██║░░░██║██╔════╝██╔════╝████╗░██║  ██╔══██╗████╗░████║██╔══██╗██║
+██║██╗██║██║░░░██║█████╗░░█████╗░░██╔██╗██║  ███████║██╔████╔██║██║░░██║██║
+╚██████╔╝██║░░░██║██╔══╝░░██╔══╝░░██║╚████║  ██╔══██║██║╚██╔╝██║██║░░██║██║
+░╚═██╔═╝░╚██████╔╝███████╗███████╗██║░╚███║  ██║░░██║██║░╚═╝░██║██████╔╝██║
+░░░╚═╝░░░░╚═════╝░╚══════╝╚══════╝╚═╝░░╚══╝  ╚═╝░░╚═╝╚═╝░░░░░╚═╝╚═════╝░╚═╝
+ __  __       _ _   _       ____             _          
+|  \/  |_   _| | |_(_)     |  _ \  _____   _(_) ___ ___ 
+| |\/| | | | | | __| |_____| | | |/ _ \ \ / / |/ __/ _ \
+| |  | | |_| | | |_| |_____| |_| |  __/\ V /| | (_|  __/
+|_|  |_|\__,_|_|\__|_|     |____/ \___| \_/ |_|\___\___|
+* @project_name Queen Amdi [WA Multi-device]
+* @author BlackAmda <https://github.com/lilkjo>
+* @description A WhatsApp based 3ʳᵈ party application that provide many services with a real-time automated conversational experience
+* @link <https://github.com/lilkjo/QueenAmdi>
+* @version 4.0.2
+* @file  amdiSettings.js - QueenAmdi config var exports
+
+© 2022 Black Amda, ANTECH. All rights reserved.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.*/
+
+const fs = require('fs');
+if (fs.existsSync('amdiSettings.env')) require('dotenv').config({ path: './assets/amdiSettings.env' });
+
+module.exports = {
+    VERSION: 'Queen Amdi 4.0.2v - Multi-Device',
+    DATABASE_URL: process.env.DATABASE_URL ===  undefined ? 'local' : process.env.DATABASE_URL,
+    LANGUAGE: process.env.LANGUAGE === undefined ? 'EN' : process.env.LANGUAGE,
+    isHEROKU: process.env.HEROKU === undefined ? '' : process.env.HEROKU,
+    HEROKU_APP: process.env.HEROKU_APP_NAME === undefined ? '' : process.env.HEROKU_APP_NAME
+}
